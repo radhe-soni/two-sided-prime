1f290f0fb4e880fe92cad9bdb64c4dc04e1b94bb
radhe-soni <rsheracles@gmail.com>
2019-12-30 05:59:09 +0000

    removed async block

--- primegenerator/prime-generator.go
+++ primegenerator/prime-generator.go
@@ -11,13 +11,18 @@ import (
 func main() {
 	log.Println("start")
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	for i := int64(30); i < 1<<20; i++ {
+	/*for i := int64(30); i < 1<<25; i++ {
 		go func(j int64) {
 			if primetest.IsTwoSidedPrime(j) {
 				fmt.Println(j)
 			}
 		}(i)
 
+	}*/
+	for i := int64(30); i < 1<<20; i++ {
+		if primetest.IsTwoSidedPrime(i) {
+				fmt.Println(i)
+			}
 	}
 	log.Println("end")
 }
